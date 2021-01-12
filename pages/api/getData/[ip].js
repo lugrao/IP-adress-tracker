1fa19e9d76ee05258c9bb124e220b69bce0c0c65
lugrao <elugrao@gmail.com>
2021-01-12 11:31:04 -0300

    agrega rutas dinámicas para obtener data de ips

--- pages/api/getData/[ip].js
+++ pages/api/getData/[ip].js
@@ -1,10 +1,8 @@
 export default async function handler(req, res) {
+  const { query: { ip } } = req;  
   const apiKey = "at_o4xhBh8I7V1hewte23RKs60EG7kl6";
-  const ip = dev ? "201.231.175.210" : req.headers["x-forwarded-for"];
   const URL = `https://geo.ipify.org/api/v1?apiKey=${apiKey}&ipAddress=${ip}`;
   const getInfo = await fetch(URL);
   const json = await getInfo.json();
   res.status(200).json(json);
 }
-
-const dev = process.env.NODE_ENV !== "production";
